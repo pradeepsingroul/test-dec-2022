073bddbb5a93f162c5c051aa962de22a12b90fac
predeep kumar singroul <prasingroul4899@gmail.com>
2023-06-05 07:25:08 +0530

    Student Service

--- TestContainerIntegration-1/src/main/java/com/RedCarpetUp/Services/StudentService.java
+++ TestContainerIntegration-1/src/main/java/com/RedCarpetUp/Services/StudentService.java
@@ -1,5 +1,18 @@
 package com.RedCarpetUp.Services;
 
+import java.util.List;
+
+import com.RedCarpetUp.Entity.Student;
+import com.RedCarpetUp.Exceptions.StudentExceptions;
+
 public interface StudentService {
+	
+	List<Student> getAllStudents() throws  StudentExceptions;
+	
+	Student registerStudent() throws  StudentExceptions;
+	
+	Student getStudentById() throws  StudentExceptions;
+	
+	Student deleteStudentById() throws StudentExceptions;
 
 }
