4314bee457c73d51db8c8310217220f5aee6b416
predeep kumar singroul <prasingroul4899@gmail.com>
2023-06-05 11:27:05 +0530

    address service

---
+++ TestContainerIntegration-1/src/main/java/com/RedCarpetUp/Services/AddressService.java
@@ -0,0 +1,9 @@
+package com.RedCarpetUp.Services;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.RedCarpetUp.Entity.Adderess;
+
+public interface AddressService extends JpaRepository<Adderess, Integer>{
+
+}
