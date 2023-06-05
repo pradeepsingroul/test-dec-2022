63737343b4ac5817a9bce92c5a7cf570bf3939d5
predeep kumar singroul <prasingroul4899@gmail.com>
2023-06-05 07:20:54 +0530

    address repository

--- TestContainerIntegration-1/src/main/java/com/RedCarpetUp/Repository/AddressRepository.java
+++ TestContainerIntegration-1/src/main/java/com/RedCarpetUp/Repository/AddressRepository.java
@@ -1,5 +1,12 @@
 package com.RedCarpetUp.Repository;
 
-public interface AddressRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.RedCarpetUp.Entity.Adderess;
+
+@Repository
+public interface AddressRepository extends JpaRepository<Adderess, Integer>{
+
 
 }
