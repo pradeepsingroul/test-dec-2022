1e7e33f24b0979e7a2ff9d37e2c2d2edfc35d315
predeep kumar singroul <prasingroul4899@gmail.com>
2023-06-05 14:37:01 +0530

    Student service updates

--- TestContainerIntegration-1/src/main/java/com/RedCarpetUp/Services/StudentService.java
+++ TestContainerIntegration-1/src/main/java/com/RedCarpetUp/Services/StudentService.java
@@ -2,17 +2,20 @@ package com.RedCarpetUp.Services;
 
 import java.util.List;
 
+import org.springframework.stereotype.Service;
+
 import com.RedCarpetUp.Entity.Student;
 import com.RedCarpetUp.Exceptions.StudentExceptions;
+
 
 public interface StudentService {
 	
 	List<Student> getAllStudents() throws  StudentExceptions;
 	
-	Student registerStudent() throws  StudentExceptions;
+	Student registerStudent(Student st) throws  StudentExceptions;
 	
-	Student getStudentById() throws  StudentExceptions;
+	Student getStudentById(Integer id) throws  StudentExceptions;
 	
-	Student deleteStudentById() throws StudentExceptions;
+	Student deleteStudentById(Integer id) throws StudentExceptions;
 
 }
--- TestContainerIntegration-1/src/main/java/com/RedCarpetUp/Services/StudentServiceImpl.java
+++ TestContainerIntegration-1/src/main/java/com/RedCarpetUp/Services/StudentServiceImpl.java
@@ -1,37 +1,53 @@
 package com.RedCarpetUp.Services;
 
 import java.util.List;
+import java.util.Optional;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import com.RedCarpetUp.Entity.Student;
 import com.RedCarpetUp.Exceptions.StudentExceptions;
+import com.RedCarpetUp.Repository.StudentRepository;
 
 @Service
 public class StudentServiceImpl implements StudentService{
 
+	@Autowired
+	private StudentRepository sRepo;
+	
 	@Override
 	public List<Student> getAllStudents() throws StudentExceptions {
-		// TODO Auto-generated method stub
-		return null;
+		List<Student> students = sRepo.findAll();
+		if(students.isEmpty())throw new StudentExceptions("Students does not exist...");
+		else return students;
 	}
 
 	@Override
-	public Student registerStudent() throws StudentExceptions {
-		// TODO Auto-generated method stub
-		return null;
+	public Student registerStudent(Student student) throws StudentExceptions {
+		Optional<Student> opt = sRepo.findById(student.getStuId());  
+		if(opt.isPresent())throw new StudentExceptions("Students already exist...");
+		else return sRepo.save(student);
 	}
 
 	@Override
-	public Student getStudentById() throws StudentExceptions {
-		// TODO Auto-generated method stub
-		return null;
+	public Student getStudentById(Integer id) throws StudentExceptions {
+		Optional<Student> opt = sRepo.findById(id);  
+		if(opt.isEmpty())throw new StudentExceptions("Students does not exist...");
+		else return opt.get();
 	}
 
 	@Override
-	public Student deleteStudentById() throws StudentExceptions {
-		// TODO Auto-generated method stub
-		return null;
+	public Student deleteStudentById(Integer id) throws StudentExceptions {
+		Optional<Student> opt = sRepo.findById(id);  
+		if(opt.isEmpty())throw new StudentExceptions("Students does not exist...");
+		else {
+			Student s = opt.get();
+		    sRepo.deleteById(id);
+			return s;
+		}
 	}
 
 }
