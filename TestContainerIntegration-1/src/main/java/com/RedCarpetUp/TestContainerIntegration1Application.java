a77ef740d49ef1c6796d28c332cdd7d0f984d731
predeep kumar singroul <prasingroul4899@gmail.com>
2023-06-05 14:41:55 +0530

    Home

---
+++ TestContainerIntegration-1/src/main/java/com/RedCarpetUp/TestContainerIntegration1Application.java
@@ -0,0 +1,13 @@
+package com.RedCarpetUp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestContainerIntegration1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestContainerIntegration1Application.class, args);
+	}
+
+}
---
+++ TestContainerIntegration-1/src/test/java/com/RedCarpetUp/TestContainerIntegration1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.RedCarpetUp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestContainerIntegration1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
