f607f2d3ebc10f9f568bdea28fe72f2a3d4993d3
predeep kumar singroul <prasingroul4899@gmail.com>
2023-06-05 14:41:02 +0530

    Student controllers

--- TestContainerIntegration-1/src/main/java/com/RedCarpetUp/controller/StudentController.java
+++ TestContainerIntegration-1/src/main/java/com/RedCarpetUp/controller/StudentController.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -30,8 +32,20 @@ public class StudentController {
 	
 	@GetMapping("/{getId}")
 	public  ResponseEntity<Student> getStudentByIdEntity(@RequestParam("getId") Integer id){
-		Student students = sdao.getStudentById(id);
-		return new ResponseEntity<Student>(students,HttpStatus.OK);
+		Student student = sdao.getStudentById(id);
+		return new ResponseEntity<Student>(student,HttpStatus.OK);
+	}
+	
+	@GetMapping("/{deleteId}")
+	public  ResponseEntity<Student> deleteStudentById(@RequestParam("deleteId") Integer id){
+		Student student = sdao.deleteStudentById(id);
+		return new ResponseEntity<Student>(student,HttpStatus.OK);
+	}
+	
+	@PostMapping("/register")
+	public  ResponseEntity<Student> deleteStudentById(@RequestBody Student student){
+		Student student1 = sdao.registerStudent(student);
+		return new ResponseEntity<Student>(student1,HttpStatus.OK);
 	}
 	
 	
