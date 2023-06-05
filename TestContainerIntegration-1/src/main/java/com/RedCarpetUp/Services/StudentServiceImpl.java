08026d8392e5b5d038fccba92b805eeb1d8efebb
predeep kumar singroul <prasingroul4899@gmail.com>
2023-06-05 07:25:59 +0530

    Student Service implementation

---
+++ TestContainerIntegration-1/src/main/java/com/RedCarpetUp/Services/StudentServiceImpl.java
@@ -0,0 +1,37 @@
+package com.RedCarpetUp.Services;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.RedCarpetUp.Entity.Student;
+import com.RedCarpetUp.Exceptions.StudentExceptions;
+
+@Service
+public class StudentServiceImpl implements StudentService{
+
+	@Override
+	public List<Student> getAllStudents() throws StudentExceptions {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Student registerStudent() throws StudentExceptions {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Student getStudentById() throws StudentExceptions {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Student deleteStudentById() throws StudentExceptions {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
